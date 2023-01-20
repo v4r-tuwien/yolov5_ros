31a6807b7fb930147394d0fa0dfd128d516195eb
Peter Hoenig <hpfreak1004@gmail.com>
2023-01-20 15:04:01 +0100

    adding docker compose file

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#docker run \
+#-it \
+#--network=host \
+#ros:noetic-ros-base
+
+echo $HOSTNAME
+
 docker run \
 --gpus all \
 -it \
@@ -7,6 +14,9 @@ docker run \
 --volume="/dev/bus/usb:/dev/bus/usb" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 --volume="/home/hoenig/iChores/yolov5:/root/yolo" \
+--net host \
+--env ROS_HOSTNAME=$HOSTNAME \
+--env ROS_MASTER_URI=http://$HOSTNAME:11311/ \
 --name=yolo_rosv0 yolo_ros
 
 #xhost +local:`docker inspect --format='{{ .Config.Hostname }}' yolo_rosv0`
