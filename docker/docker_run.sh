409fbb9f0dd73e91a71f23b2512faa18f6eac8d7
Peter Hoenig <hpfreak1004@gmail.com>
2023-05-05 13:57:36 +0200

    changed docker base image to reduce container size to about 12gb instead of 15

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -13,7 +13,7 @@ docker run \
 --shm-size=8gb --env="DISPLAY" --privileged \
 --volume="/dev/bus/usb:/dev/bus/usb" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---volume="$HOME/iChores/yolov5:/root/yolo" \
+--volume="$HOME/iChores/yolov5_ros:/root/yolo" \
 --net host \
 --env ROS_HOSTNAME=$HOSTNAME \
 --env ROS_MASTER_URI=http://$HOSTNAME:11311/ \
